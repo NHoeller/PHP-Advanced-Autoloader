ff52916f1476f52aa70b1ce6e51b418209f653de
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2016-02-09 23:29:36 +0100

    file construction
    this file will be filled with huge comment blocks to explain how it works.

---
+++ AdvancedAutoloader.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 
+ * define namespace
+ *
+ */
+namespace Conceptixx\AdvancedAutloader
+
+/**
+ *
+ * AdvancedAutoloader
+ *
+ * this is the base class of the PHP-Advanced-Autoloader system. it is used to implement the
+ * self extending and optimizing autoloader system. if the requirement of the conceptixx
+ * PHP-Advanced-Autoloader is followed there need not to be any changes. in a further version
+ * of this autoloader system an automatic update with this ithub repository is planned.
+ *
+ */
+class AdvancedAutoloader
+{
+
+    /**
+     * USED_STANDARDS
+     *
+     * this constant contains all featured 'standard' this system is using. all values are
+     * comma separated and handled by the autoloader system.
+     * 
+     * @var constant USED_STANDARDS
+     */
+    const USED_STANDARDS = 'vendor,extend,mapping,psr4,psr0,pear';
+
+// TODO: to be continued
+}
